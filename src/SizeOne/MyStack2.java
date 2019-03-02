138dd42d4321a1edf59b4b6420ae22e62707ff6d
apan <1285911832@qq.com>
2019-03-02 12:46:10 +0800

    第二题

---
+++ src/SizeOne/MyStack2.java
@@ -0,0 +1,33 @@
+package SizeOne;
+/**
+ * 如何仅用递归函数和栈操作逆序一个栈
+ */
+
+import java.util.Stack;
+
+/**
+ *递归：先写退出条件，然后再写主体
+ */
+public class MyStack2 {
+    private Stack<Integer> stack1;
+
+    public MyStack2() {
+        this.stack1 = new Stack();
+    }
+
+    public Stack<Integer> Reverse(Stack<Integer> stack) {
+        if (stack.isEmpty() == true)
+            return this.stack1;
+        this.stack1.push(stack.pop());
+        return Reverse(stack);
+    }
+
+    public static void main(String[] args) {
+        MyStack2 myStack2 = new MyStack2();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= 5; i++)
+            stack.push(i);
+        System.out.println(stack.toString());
+        System.out.println(myStack2.Reverse(stack).toString());
+    }
+}
---
+++ src/SizeOne/MyStack3.java
@@ -0,0 +1,12 @@
+package SizeOne;/**
+ * 题目：
+ */
+
+/**
+ *
+ */
+public class MyStack3 {
+    public static void main(String args[]) {
+
+    }
+}
