aedb4e5429e9ef2045540700cffff09176f2159d
apan <1285911832@qq.com>
2019-03-02 19:57:23 +0800

    3

--- src/SizeOne/MyStack3.java
+++ src/SizeOne/MyStack3.java
@@ -1,12 +1,27 @@
-package SizeOne;/**
- * 题目：
+package SizeOne;
+import java.util.Stack;
+import java.util.Scanner;/**
+ * 题目：求字符串最后一个单词的长度
  */
 
 /**
- *
+ *思考：找准数据结构来存储，空间换时间
  */
-public class MyStack3 {
-    public static void main(String args[]) {
-
+public class MyStack3{
+    public static int hello(String words){
+        Stack<Character> stack = new Stack();
+        char[] chars = words.toCharArray();
+        for(char _char : chars){
+            if(Character.isSpaceChar(_char)){
+                stack.clear();
+                continue;
+            }
+            stack.push(_char);
+        }
+        return stack.size();
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(hello(sc.nextLine()));
     }
 }
